0dd0a469ad45650bc7679b43f8d41d9e852caf8b
Christian Kaisermann <christian@kaisermann.it>
2015-08-26 15:16:23 -0300

    Commit 0.5

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Visibility State JS
+# ViSC - Visibility State Controller JS
 
-IE 9+
-jQuery, Zepto compatible
+## Compatibility 
+- IE 9+
+- jQuery & Zepto Compatible
+
+## How To Use
+###Vanilla JS
+> var visc = new Visc();
+> visc.bind(element_list, function(states) {});
+> visc.unbind();
+> Visc.getState(element_list);
+
+###jQuery / Zepto Methods
+> $('selector').visc(function(states){})
+> $('selector').visc('getState');
+> $('selector').unvisc();
--- visibilityjs.js
+++ visibilityjs.js
@@ -1,146 +1,46 @@
 /*!
- * ViSC - Visibility State Controller JS v0.3.5.2
- * Elements Visibility State
+ * ViSC - Visibility State Controller JS v0.5
+ * Elements Visibility State Controller
+ * https://github.com/chriskaisermann/ViSC
  * by Christian Kaisermann
  */
 
  /* global jQuery */
  /* global Zepto */
 
- (function(window, undefined)
+ (function(window, $, undefined)
  {
  	'use strict';
 
- 	/* -- Visibility State Controller Class -- */
+ 	var
+ 	_Slice = Array.prototype.slice,
+ 	_win = {},
+ 	_client = {},
+ 	_instanced = 0;
 
- 	var ViSC = function()
+ 	/* Private Window Methods */
+ 	var 
+ 	bindWindowObserver = function() 
+ 	{ 
+ 		window.addEventListener("resize", updateWindowSize);
+ 		window.addEventListener("scroll", updateWindowSize);
+ 	},
+ 	unbindWindowObserver =function () 
  	{
- 		var self = this;
- 		self._isActive = false;
-
- 		function init()
- 		{
- 			self._clientTop = document.documentElement.clientTop;
- 			self._clientLeft = document.documentElement.clientLeft;
- 			
- 			self.bindEvents();
- 		}
-
- 		self.bindEvents = function()
- 		{
- 			self._isActive = true;
- 			window.addEventListener("resize", windowTransformationHandler);
- 			window.addEventListener("scroll", windowTransformationHandler);
- 		}
-
- 		self.unbindEvents = function() 
- 		{
- 			self._isActive = false;
- 			window.removeEventListener("resize", windowTransformationHandler);
- 			window.removeEventListener("scroll", windowTransformationHandler);
- 		}
-
- 		function windowTransformationHandler()
- 		{
- 			self._win = new Rectangle( getPosHorizontalScroll(), getPosVerticalScroll(), window.innerWidth, window.innerHeight);
- 		}
-
- 		function getPosHorizontalScroll() { return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft; }
- 		function getPosVerticalScroll() { return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop; }
-
- 		init();
+ 		window.removeEventListener("resize", updateWindowSize);
+ 		window.removeEventListener("scroll", updateWindowSize);
+ 	},
+ 	updateWindowSize = function()
+ 	{
+ 		_client = { top: document.documentElement.clientTop, left: document.documentElement.clientLeft };
+ 		_win = new Rectangle(
+ 			window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+ 			window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop, 
+ 			window.innerWidth, window.innerHeight);
  	};
-
- 	ViSC.prototype.getState = function(element)
- 	{
- 		if(!this._isActive || element === undefined)
- 			return null;
-
- 		var _eType = element.constructor.name || null;
-
- 		var 
- 		states = [],
- 		elements = [];
-
- 		if(!!window.jQuery  && element instanceof jQuery)
- 			elements = jQuery.makeArray(element);
- 		else if(_eType==="NodeList")
- 			elements = Array.prototype.slice.call(element, 0);
- 		else if(Array.isArray(element))
- 			elements = element;
- 		else if(element.nodeType)
- 			elements.push(element);
- 		else
- 			return null;
-
- 		for(var i = 0; i < elements.length; i++)
- 		{
- 			var _e = elements[i],
- 			_frame = this.getOffsetRect(_e),
- 			_intersection = _frame.intersectionWith(this._win);
-
- 			var state = new VisibilityState();
-
- 			if(_intersection)
- 			{
- 				var 
- 				_intersectionArea = _intersection.getArea(),
- 				_minWidth = Math.min(_frame.width, this._win.width),
- 				_minHeight = Math.min(_frame.height, this._win.height);
-
- 				state.visibilityRate = {
- 					both: _intersectionArea / _frame.getArea(),
- 					horizontal: _intersection.width / _frame.width,
- 					vertical: _intersection.height / _frame.height
- 				};
-
- 				state.occupiedViewport = {
- 					both: _intersectionArea / this._win.getArea(),
- 					horizontal: _intersection.width / this._win.width,
- 					vertical: _intersection.height / this._win.height
- 				};
-
- 				state.maxVisibility =
- 				{
- 					both: _intersectionArea / (_minWidth * _minHeight),
- 					horizontal: _intersection.width / _minWidth,
- 					vertical: _intersection.height / _minHeight	
- 				};
-
- 			}
- 			if(elements.length===1)
- 				return state;
- 			states.push(state);
- 		}
- 		return states;
- 	};
-
- 	ViSC.prototype.getOffsetRect = function(element) 
- 	{
- 		var box = element.getBoundingClientRect();
- 		var top = box.top + this._win.top - this._clientTop;
- 		var left = box.left + this._win.left - this._clientLeft;
-
- 		return new Rectangle(left, top, box.width, box.height);
- 	};
-
- 	ViSC.prototype.sleep = function() { this.unbindEvents(); }
- 	ViSC.prototype.wake = function() { this.bindEvents(); }
-
- 	/* -- Visibility State Controller Class -- */
-
- 	/* -- Visibility State Class -- */
-
- 	function VisibilityState ()
- 	{
- 		this.visibilityRate = {both:0,horizontal:0,vertical:0};
- 		this.occupiedViewport = {both:0,horizontal:0,vertical:0};
- 		this.maxVisibility = {both:0,horizontal:0,vertical:0};
- 	}
- 	/* -- Visibility State Class -- */
+ 	/* -- Private Window Methods -- */
 
  	/* -- Rectangle Class -- */
-
  	function Rectangle(x, y, w, h)
  	{
  		this.left = Math.round(x);
@@ -164,9 +64,179 @@
  		width = right - left,
  		height = bottom - top;
 
- 		return (width >= 0 && height >= 0) ? new Rectangle(left, top, width, height) : null;
+ 		return (width >= 0 && height >= 0)?new Rectangle(left, top, width, height):null;
  	};
  	/* -- Rectangle Class -- */
 
- 	window.ViSC = ViSC;
- })(window);
+ 	/* -- Visibility State Controller Class -- */
+
+ 	var Visc = function() 
+ 	{
+ 		var _self = this,
+ 		__elements = [],
+ 		__binded = false,
+ 		__callback;
+
+ 		/* -- Private Helper Methods -- */
+ 		var 
+ 		getOffsetRect = function(element) 
+ 		{
+ 			var box = element.getBoundingClientRect();
+ 			var top = box.top + _win.top - _client.top;
+ 			var left = box.left + _win.left - _client.left;
+
+ 			return new Rectangle(left, top, box.width, box.height);
+ 		},
+ 		getElements = function(elements)
+ 		{
+ 			var _eType = elements.constructor.name || null;
+ 			__elements = [];
+ 			if(typeof elements === "string")
+ 				__elements = _Slice.call(document.querySelectorAll(elements), 0);
+ 			if(!!window.jQuery  && elements instanceof jQuery)
+ 				__elements = jQuery.makeArray(elements);
+ 			else if(_eType==="NodeList")
+ 				__elements = _Slice.call(elements, 0);
+ 			else if(Array.isArray(elements))
+ 				__elements = elements;
+ 			else if(elements.nodeType)
+ 				__elements.push(elements);
+ 			else
+ 				return null;
+ 		},
+ 		windowChanged = function()
+ 		{
+ 			if(!!__callback)
+ 				__callback.call(_self,_self.getState(__elements));
+ 		};
+ 		/* -- Private Helper Methods -- */
+
+ 		/* -- Privilleged Methods -- */
+ 		_self.bind = function(element, callback)
+ 		{
+ 			if(!_instanced++)
+ 				bindWindowObserver();
+
+ 			if(typeof callback === 'function')
+ 				__callback = callback;
+ 			else
+ 				console.error("[Visc: Invalid Callback]");
+ 			getElements(element);
+
+ 			__binded = true;
+ 			window.addEventListener("resize", windowChanged);
+ 			window.addEventListener("scroll", windowChanged);
+ 		};
+
+ 		_self.unbind = function () 
+ 		{
+ 			_self.__elements = [];
+
+ 			__binded = false; 
+ 			window.removeEventListener("resize", windowChanged);
+ 			window.removeEventListener("scroll", windowChanged);
+
+ 			if(!--_instanced) 
+ 				unbindWindowObserver(); 
+ 		}
+
+ 		_self.getState = function(elements) 
+ 		{
+ 			if(elements === undefined)
+ 				return null;
+
+ 			if(!__elements.length)
+ 				getElements(elements);
+
+ 			if(!__binded)
+ 				updateWindowSize();
+
+ 			var states = []; 		
+
+ 			for(var i = 0; i < __elements.length; i++)
+ 			{
+ 				var _e = __elements[i],
+ 				_frame = getOffsetRect(_e),
+ 				_intersection = _frame.intersectionWith(_win);
+
+ 				var state = new VisibilityState(_e);
+
+ 				if(_intersection)
+ 				{
+ 					var 
+ 					_intersectionArea = _intersection.getArea(),
+ 					_minWidth = Math.min(_frame.width, _win.width),
+ 					_minHeight = Math.min(_frame.height, _win.height);
+
+ 					state.visibilityRate = {
+ 						both: _intersectionArea / _frame.getArea(),
+ 						horizontal: _intersection.width / _frame.width,
+ 						vertical: _intersection.height / _frame.height
+ 					};
+
+ 					state.occupiedViewport = {
+ 						both: _intersectionArea / _win.getArea(),
+ 						horizontal: _intersection.width / _win.width,
+ 						vertical: _intersection.height / _win.height
+ 					};
+
+ 					state.maxVisibility =
+ 					{
+ 						both: _intersectionArea / (_minWidth * _minHeight),
+ 						horizontal: _intersection.width / _minWidth,
+ 						vertical: _intersection.height / _minHeight	
+ 					};
+
+ 				}
+ 				states.push(state);
+ 			}
+ 			return states;	
+ 		};
+ 		/* -- Privilleged Methods -- */
+
+ 		updateWindowSize();
+ 	};
+
+
+ 	/* -- Public Static Methods -- */
+ 	Visc.getNumberOfInstances = function () { return _instanced; };
+ 	Visc.getState = function (elements) { return new Visc().getState(elements); }
+ 	/* -- Public Static Methods -- */
+ 	/* -- Visibility State Controller Class -- */
+
+ 	/* -- Visibility State Class -- */
+ 	function VisibilityState (element)
+ 	{
+ 		this.visibilityRate = {both:0,horizontal:0,vertical:0};
+ 		this.occupiedViewport = {both:0,horizontal:0,vertical:0};
+ 		this.maxVisibility = {both:0,horizontal:0,vertical:0};
+ 		this.element = element;
+ 	}
+ 	/* -- Visibility State Class -- */
+
+ 	/* -- jQuery / Zepto Plugin -- */
+ 	if(!!$)
+ 	{
+ 		$.fn['visc'] = function (method) 
+ 		{
+ 			if(method==="getState")
+ 			{
+ 				return Visc[method].call(this, this);
+ 			}
+ 			else if (typeof method === 'function' || !method) 
+ 			{
+ 				var vsc = new Visc();
+ 				this.data('visc', vsc);
+ 				vsc.bind(this, arguments[0]);
+ 				return this;
+ 			}
+ 			else 
+ 				$.error( 'Method "' +  method + '"" does not exist on $.ViSC' );
+ 		};
+
+ 		$.fn['unvisc'] = function () { return this.data('visc').unbind(); }
+ 	}
+ 	/* -- jQuery / Zepto Plugin -- */
+
+ 	window.Visc = Visc;
+ })(window, window.jQuery || window.Zepto || window.$);
